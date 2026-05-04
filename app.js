d3f1196d45662ca87e3ff5e866f402c5b1bdd620
Bru_dev <bruceniyigena8@gmail.com>
2026-05-04 13:45:23 +0300

    Explained js

---
+++ app.js
@@ -0,0 +1,42 @@
+
+// // Utilities:small helper we reuse in our code
+// function greet(name) {
+//     return `Hello,  ${name}!`;
+// }
+// console.log(greet("Bruce Wayne"));
+
+// // DOM: This how we Interact with the Html & Css In Javascript
+// // document.getElementById("add-note-btn").textContent = "Add Note";
+
+// // Variable: This is used to store data in Javascript
+// let name = "John Doe";
+// let age = 25;
+// let isStudent = true;
+// let text = "Hello Word";
+// let person = { name: "James", age: 25, isStudent: true };
+// let notes = ["Note 1", "Note 2", "Note 3"];
+// let number = 42;
+
+// // EventListeners: This how we react to user 
+
+
+
+// // Object: This is used to store data in Javascript || a Collection of related data
+// let student = {
+//     name: "James",
+//     subject: "Software Development",
+//     grade: 8,
+// }
+
+// let grade = "A";
+
+// if (grade === "A") {
+//     console.log("You have passed the course");
+// } else {
+//     console.log("You have failed the course");
+// }
+
+// Loops
+// for(let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
