a6cc03e5aa6fcc361f5add0463638f9de73b3c97
Bru_dev <bruceniyigena8@gmail.com>
2026-05-06 13:13:28 +0300

    All subject Card were Rendered by The codes of Java

---
+++ data.js
@@ -0,0 +1,27 @@
+// This will replace Hardcoded HTML cards
+
+const subjects = [{
+    id: 1,
+    name: "Javascript Fundamentals",
+    code: "SWDJF 301",
+    credits: 3,
+    description: "Variables, Functions, Arrays, DOM's, Events, Objects, Loops, Conditionals, etc.",
+}, {
+    id: 2,
+    name: "Vue.js Framework",
+    code: "SWDVF 301",
+    credits: 3,
+    description: "Components, Routers, Statemanagement, etc.",
+}, {
+    id: 3,
+    name: "Version Control",
+    code: "SWDVC 301",
+    credits: 3,
+    description: "Git, Github, Branching, Merging, Collaboration, etc.",
+}, {
+    id: 4,
+    name: "Website Development",
+    code: "SWDWD 301",
+    credits: 3,
+    description: "HTML5, CSS3, Responsive Design, Accessibility, etc.",
+}]
